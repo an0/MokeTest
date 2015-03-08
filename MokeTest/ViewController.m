6928ab17b1def109560a0dbb778ddc5dfa7a8d5f
Ling Wang <an00na@gmail.com>
2015-03-08 14:55:18 -0500

    Minimal Reproducible Example

--- MokeTest/ViewController.m
+++ MokeTest/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    NSArray *_URLs;
+    NSMutableSet *_connections;
+    UIBackgroundTaskIdentifier _bgTask;
+}
 
 @end
 
@@ -16,12 +20,75 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _URLs = @[
+                  @"https://api.weibo.com/2/statuses/home_timeline.json?&count=50",
+                  @"https://api.weibo.com/2/statuses/count.json?&ids=3817139716823737",
+                  @"https://api.weibo.com/2/statuses/repost_timeline.json?id=3817139716823737&count=50",
+                  @"https://api.weibo.com/2/comments/show.json?id=3817139716823737&count=50",
+                  @"https://api.weibo.com/2/users/show.json?screen_name=an00na",
+                  @"https://api.weibo.com/2/statuses/user_timeline.json?screen_name=an00na&count=50",
+                  @"https://api.weibo.com/2/remind/unread_count.json?unread_message=1",
+                  ];
+    
+    _connections = [NSMutableSet set];
+    
+    [self sendRequest];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)sendRequest {
+    NSString *urlStr = _URLs[arc4random_uniform((u_int32_t)(_URLs.count))];
+    NSURL *url = [NSURL URLWithString:urlStr];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:15];
+#warning 请指定 access token
+    NSString *accessToken = @"";
+    NSString *value = [NSString stringWithFormat:@"OAuth2 %@", accessToken];
+    [request setValue:value forHTTPHeaderField:@"Authorization"];
+
+    NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately:YES];
+    [_connections addObject:connection];
+    
+    _bgTask = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
+        if (_bgTask != UIBackgroundTaskInvalid) {
+            [[UIApplication sharedApplication] endBackgroundTask:_bgTask];
+            _bgTask = UIBackgroundTaskInvalid;
+        }
+    }];
+}
+
+#pragma mark - NSURLConnection delegate methods
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSHTTPURLResponse *)response {
+    NSLog(@"Response %@ received", response);
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    NSLog(@"Request didFailWithError: %@", error);
+    if ([error.domain isEqualToString:NSURLErrorDomain] && error.code == NSURLErrorTimedOut) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:error.localizedDescription delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+    }
+    
+    [_connections removeObject:connection];
+    if (_connections.count == 0) {
+        if (_bgTask != UIBackgroundTaskInvalid) {
+            [[UIApplication sharedApplication] endBackgroundTask:_bgTask];
+            _bgTask = UIBackgroundTaskInvalid;
+        }
+        [self performSelector:@selector(sendRequest) withObject:nil afterDelay:5];
+    }
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    NSLog(@"Request didFinishLoading: %@", connection.originalRequest.URL.path);
+    [_connections removeObject:connection];
+    if (_connections.count == 0) {
+        if (_bgTask != UIBackgroundTaskInvalid) {
+            [[UIApplication sharedApplication] endBackgroundTask:_bgTask];
+            _bgTask = UIBackgroundTaskInvalid;
+        }
+        [self performSelector:@selector(sendRequest) withObject:nil afterDelay:10];
+    }
 }
 
 @end
